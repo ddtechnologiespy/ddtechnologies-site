49b878b4bf591da2ec312c3693e5ca486c10c544
ddtechnologiespy <davalosdavalostech@gmail.com>
2025-10-19 20:52:59 -0300

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,1 @@
+document.addEventListener('DOMContentLoaded', function(){document.querySelectorAll('a[href^="#"]').forEach(a=>{a.addEventListener('click', function(e){e.preventDefault();document.querySelector(this.getAttribute('href')).scrollIntoView({behavior:'smooth'});});});});
